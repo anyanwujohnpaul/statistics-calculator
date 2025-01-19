9dc2e253b87a32733f940f4930e7e6fb0cb53775
kvngjaypee <johnpaulanyanwu17@gmail.com>
2025-01-19 21:04:23 +0100

    frustrated rn

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+const getMean = (array) =>
+  array.reduce((acc, el) => acc + el, 0) / array.length;
 
-const testArr1 = [1, 2, 3, 4, 5];
-const testArr2 = [1, 2, 3, 4, 5, 6];
-const isEven = testArr2.length % 2 === 0;
-console.log(isEven);
-const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
-console.log(oddListMedian);
-const evenListMedian = getMean([testArr2[testArr2.length / 2 - 1], testArr2[testArr2.length / 2]]);
-console.log(evenListMedian);
 const getMedian = (array) => {
   const sorted = array.sort((a, b) => a - b);
-}
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
+  return median;
+};
+
+const getMode = (array) => {
+  const counts = {};
+  array.forEach((el) => {
+    if (counts[el]) {
+      counts[el] += 1;
+    } else {
+      counts[el] = 1;
+    }
+  });
+  return counts;
+};
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
-  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-  
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+
   const mean = getMean(numbers);
+  const median = getMedian(numbers);
+  console.log(getMode(numbers));
 
   document.querySelector("#mean").textContent = mean;
-}
+  document.querySelector("#median").textContent = median;
+};
